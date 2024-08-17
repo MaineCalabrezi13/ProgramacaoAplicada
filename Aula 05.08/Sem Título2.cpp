1f7eb6e8bda57ac9d8cfc23c18620d1923d24aee
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2024-08-17 19:56:29 -0300

    Add files via upload

---
+++ Aula 05.08/Sem Título2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	int a = 5;
+	double b = 3.14;
+	char c = 'A';
+	
+	int soma = a + 2;
+	double area = b * a;
+	
+	std::cout << "Soma: " << soma << std::endl;
+	std::cout << "Area: " << area << std::endl;
+	std::cout << "Caracter: " << c << std::endl;
+}
---
+++ Aula 05.08/Sem Título3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string nome;
+	std::cout << "Digite seu nome: ";
+	std::cin >> nome;
+	int idade;
+	std::cout << "Digite sua idade: ";
+	std::cin >> idade;
+	std::cout << "Ola, "<< nome << "! Voce tem "<< idade << " anos." ;
+
+}
